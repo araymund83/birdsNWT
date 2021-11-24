d4657e2dc15608483bd759839bbfc1d0d00338ba
araymund83 <araymund83@gmail.com>
2021-11-24 19:02:41 +0000

    feat- added a line to rbind all lists

--- R/speciesTable.R
+++ R/speciesTable.R
@@ -1,13 +1,20 @@
-speciesTable <- function(stack){
-  message(crayon::green ('Creating data frame for:',  ))
+speciesTable <- function(list, 
+                         species, 
+                         gcm){
+  
+  message(crayon::green ('Creating data table for:', sp))
+  dtEachSp <- data.table(pixelID = 1:ncell(sp), getValues(sp))
+  dtEachSp <- na.omit(dtEachSp)
+  retrun(dtEachSp)
     
-    allStack <- flatten(meanStack)
-    allStack <- stack(allStack)
-    names(allStack) <- gsub('mean_', '', names(allStack))
-   
-    allTble <- rasterToPoints(allStack, spatial = FALSE)
-    allTble <- as_tibble(allTble)
-    
-    return(allTble)
 }
- 
+
+unlist(allStack)
+
+dtSp <- lapply(X = allStack, FUN = function(sp){
+  dtEachSp <- data.table(pixelID = 1:ncell(sp), getValues(sp))
+  dtEachSp <- na.omit(dtEachSp)
+  return(dtEachSp)
+})
+
+allSpDT <- rbindlist(dtSp, fill = T, use.names = T)
