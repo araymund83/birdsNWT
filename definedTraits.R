8a00ce9b5e9dd75f38012e86193c8cf02c87e182
araymund83 <araymund83@gmail.com>
2021-12-14 11:30:20 -0800

    feat- traits and scatter plot for ecoregions

---
+++ definedTraits.R
@@ -0,0 +1,40 @@
+# Define traits  ----------------------------------------------------------
+### traits for migratory strategy
+#residents
+residents <- c('ATTW', 'BBWO', 'BCCH', 'BOCH', 'CORA', 'GRAJ', 'PIWO', 'RBNU',
+               'RUGR')
+#short distant migrants
+sdm <- c('AMCR', 'AMRO', 'ATSP', 'BHCO', 'BHVI', 'BRBL', 'BRCR', 'CHSP', 'DEJU',
+         'FOSP', 'GCKI', 'HETH', 'HOLA', 'LCSP', 'LEYE', 'LISP','NOFL', 'OCWA',
+         'PAWA', 'PUFI', 'RCKI', 'RUBL', 'RWBL' ,'SAVS', 'SOSP', 'SWSP', 'TRES',
+         'VATH', 'WCSP', 'WIWR', 'WTSP', 'YBSA', 'YRWA')
+#neotropical migrants
+nm <- c('ALFL', 'AMRE', 'BARS', 'BAWW', 'BBWA', 'BLPW', 'BTNW', 'CAWA', 'CCSP', 
+        'CONW', 'COYE', 'EAKI' ,'GCTH', 'HAFL', 'LEFL', 'MAWA', 'NOWA' , 'OSFL',
+        'OVEN', 'PHVI', 'RBGR', 'REVI','SWTH', 'TEWA', 'WAVI', 'WETA', 'WEWP',
+        'WIWA', 'YBFL', 'YEWA')
+#nomadic
+nomadic <- c('EVGR', 'PISI', 'WWCR')
+#------------------------------------------------------------------------------
+### traits for NESTING HABITAT
+#agricultural
+agri <- c('BARS', 'BHCO','BRBL', 'HOLA', 'SAVS')
+#bogs
+bogs <- c('ATSP', 'CONW', 'LISP','OSFL', 'PAWA', 'TEWA', 'WCSP', 'YBFL')
+#coniferous woodlands 
+coni<- c('BBWA', 'BBWO', 'BLPW','BOCH','BTNW', 'DEJU', 'GCKI', 'GCTH', 'GRAJ',
+         'HAFL', 'HETH', 'PISI', 'RCKI', 'WETA','WIWR', 'WWCR', 'YRWA')
+#deciduous woodlands
+deci <- c('AMRE', 'CAWA','LEFL', 'OVEN', 'PHVI', 'RBGR', 'REVI')
+#ground
+ground <- 'LEYE'
+#marshal
+marshal<- c('COYE', 'RWBL', 'SWSP')
+meadow <- 'LCSP'
+#mixed woodlands
+mix <- c('BAWW','BCCH', 'BHVI', 'BRCR', 'EVGR', 'MAWA', 'NOFL', 'PIWO', 'PUFI',
+         'RBNU', 'RUGR', 'SWTH', 'YBSA')
+
+#
+
+
---
+++ makeZonalGraph.R
@@ -0,0 +1,41 @@
+# Load libraries ----------------------------------------------------------
+library(pacman)
+pacman::p_load(glue, raster, rgdal, rgeos, readxl, stringr, sf, R.filesets,
+               tidyverse, terra, foreach, fs, future.apply, furrr, fst, exactextractr,
+               stringr, glue, compiler, hrbrthemes, gtools, ggpubr, gridExtra, hrbrthemes, colorspace)
+
+rm(list = ls())
+
+# Load data ---------------------------------------------------------------
+fles <- dir_ls('./qs/zonal', regexp = 'log')
+
+# Function ----------------------------------------------------------------
+make_graph <- function(fle){
+  
+  #fle <- fles[1]
+  spc <- str_sub(basename(fle), 1, 4)
+  cat('Start\n')
+  tbl <- qs::qread(file = fle)
+  tbl
+  grp <- ggplot(data = tbl, aes(x = region, y = average, col = model)) + 
+    geom_point(size = 3) + 
+    scale_color_manual(values = c('CanESM2' = "#FF6A00",'CCSM4' = "#C15CCB", 
+                                   'INM-CM4' = "#00868B")) +
+    coord_flip() +
+    ggtitle(label = glue('Zonal mean log2 density {spc}')) +
+    #theme_ipsum_es() + 
+    theme_bw() +
+    theme(legend.position = 'bottom', 
+          plot.title = element_text(size = 14)) + 
+    labs(x = 'Ecoregion', y = 'Mean', col = '')
+  
+  ggsave(plot = grp,
+         filename = glue('./graphs/figs/zonal_ecoregions/zonalEco2_{spc}.png'), 
+         units = 'in', width = 12, height = 9, dpi = 300)
+  
+  cat('Done!\n')
+}
+
+# Apply the function ------------------------------------------------------
+
+map(.x = fles, .f = make_graph)
