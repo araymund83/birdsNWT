9f40c8001a61e10a3a38cc209673dfae8d5427e7
araymund83 <araymund83@gmail.com>
2022-01-06 21:50:32 -0800

    feat- function to load the probability of ocurrence rasters.R

---
+++ R/loadOccRas.R
@@ -0,0 +1,29 @@
+loadOccRas<- function(species, 
+                       pathData,
+                       pattern, 
+                       gcm= NULL,
+                       years = NULL){
+  
+  pathData <- reproducible::checkPath(file.path(pathData), create = TRUE)
+
+  rasAvailable <- lapply(X = species, FUN = function(sp){
+    message(crayon::green('Loading files for', sp))
+    subdir<-glue(pathData,'/',sp,'/ocurr')
+    allFiles <- list.files(path = subdir, pattern = pattern, 
+                           full.names = TRUE)
+    gcm <- str_sub(basename(allFiles), start = 6, end = nchar(basename(allFiles)) - 9)
+    gcm <- unique(gcm)
+    
+    gcmAvailable <- map(.x = 1:length(gcm), .f = function(gc){
+      message(crayon::green('Creating time series for', gcm[gc]))
+      files <- grep(gcm[gc], allFiles, value = TRUE)
+      gcmFiles <- lapply(files, raster::raster)
+      gcmStack <- raster::stack(gcmFiles)
+      
+    })
+    names(gcmAvailable) <- gcm
+    return(gcmAvailable)
+    return(rasAvailable)
+  })
+}
+
