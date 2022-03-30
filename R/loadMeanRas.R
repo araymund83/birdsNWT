15e54bd8a811e26f6784e273372b2cf7c74cae75
Ana Ray <araymund83@gmail.com>
2022-03-30 00:09:46 -0700

    feat- change to grep options to stack rasters

--- R/loadMeanRas.R
+++ R/loadMeanRas.R
@@ -9,10 +9,10 @@ loadMeanRas<- function(species,
  dirs <- fs::dir_ls(pathData, type = 'directory')
  meansAvailable <- lapply(X = species, FUN = function(sp){
    message(crayon::green('Loading files for', sp))
- allFiles <- usefulFuns::grepMulti(x= list.files(path = dirs, full.names = TRUE), 
-                                   patterns = paste(pattern, sp, sep = '_'))
- gcm <- str_sub(basename(allFiles), start = 16, end = nchar(basename(allFiles)) - 4)
- gcm <- unique(gcm)
+  fls <- list.files(dirs, pattern = pattern, full.names = TRUE)
+  allFiles <- grep(sp, fls, value = TRUE)
+  gcm <- str_sub(basename(allFiles), start = 16, end = nchar(basename(allFiles)) - 4)
+  gcm <- unique(gcm)
  
  gcmAvailable <- map(.x = 1:length(gcm), .f = function(gc){
     message(crayon::green('Creating time series for', gc))
