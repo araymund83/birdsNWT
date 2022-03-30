93d23486202d7a95ea0e7ac0801389b8659a00c7
Ana Ray <araymund83@gmail.com>
2022-03-30 00:13:17 -0700

    feat- group Total graph and scatter plot code for groups

--- time_series_plots.R
+++ time_series_plots.R
@@ -13,9 +13,14 @@ change <- qs::qread('./tables/yr_changeTable.qs')
 totalTable <- qs::qread(file = glue('./tables/totalTable.qs'))
 abundance<- qs::qread( './tables/totalAbundance.qs')
 
+sum <- abundance |> 
+  filter(year %in% c(2011, 2091)) |> 
+  spread(year, value) |> 
+  setNames(c('specie', 'gcm', 'y2011', 'y2091'))
+
 traits <- read.csv('./inputs/birdSpecies_traits.csv')
 traits2 <- traits %>% dplyr::select(Group,species,Family)
-colnames(traits2) <- c('Group', 'species', 'Family')
+colnames(traits2) <- c('Group', 'specie', 'Family')
 
 
 sppAll <- left_join(totalTable, traits2, by = 'species')
@@ -30,7 +35,7 @@ summaryGroup <- sppGroup %>%  group_by (gc, Year, Group) %>%
                summarise(across(everything(), list(sum))) %>% 
   ungroup()
   
-data <- left_join(sum, traits2, by = 'specie')
+
 
 sppGroup %>% 
   filter(Group %in% c('Forest', 'Grasslands')) %>% 
@@ -57,6 +62,8 @@ ggsave(plot = totalPlot, filename = glue('./graphs/figs/yrChange/totalGroup.png'
        units = 'in', width = 12, height = 9, dpi = 700)
 
 
+data <- left_join(sum, traits2, by = 'specie')
+
 # Making the scatterplot -------------------------------------------------
 
 x <- 2011
@@ -70,9 +77,9 @@ y <- 2031
 colnames(traits2)<- c('Group', 'specie', 'Family')
 abundance <- left_join(abundance, traits2,by= 'specie' )
 sum <- abundance |> 
-  filter(year %in% c(2011, 2031)) |> 
+  filter(year %in% c(2011, 2091)) |> 
   spread(year, value) |> 
-  setNames(c('specie', 'gcm', 'Group', 'Family', 'y2011', 'y2031'))
+  setNames(c('specie', 'gcm', 'Group', 'Family', 'y2011', 'y2091'))
 
 data <- qs::qread(file = './tables/totalAbundance1191.qs')
 data <-sum
@@ -81,25 +88,25 @@ data <-sum
 make_graph <- function(data){
   
   yr1 <- '2011'
-  yr2 <- '2031'
+  yr2 <- '2091'
   gcm <- unique(data$gcm)
   corrTable <- map(.x = 1:length(gcm), .f = function(gc){
     message(crayon::green('Loading files for', gcm[gc]))
     tble <- data |> filter(gcm == gcm[gc])
     corl <- tble |> 
       group_by(gcm) |> 
-      summarise(corr = cor(y2011,y2031, method = 'pearson')) |> 
+      summarise(corr = cor(y2011,y2091, method = 'pearson')) |> 
       ungroup() |> 
       mutate(corr = round(corr, 2))
     
     
     cat('Making the correlation graph\n')
     
-    gsct <- tble %>%  ggplot(aes(x = y2011, y = y2031, color = Group)) + 
+    gsct <- tble %>%  ggplot(aes(x = y2011, y = y2091, color = Group)) + 
       geom_point(aes(color = Group, shape = Group), 
                  size = 2, alpha = 0.8) +
-      scale_color_manual(values = c( "#FF6A00","#C15CCB",  "#00868B")) +
-      ggrepel::geom_text_repel(aes(label = specie), size = 4,
+      #scale_color_manual(values = c( "#FF6A00","#C15CCB",  "#00868B")) +
+      ggrepel::geom_text_repel(aes(label = specie), size = 5,
                       # min.segment.length = 0,
                       # seed = 42,
                       # box.padding = 0.5,
@@ -116,36 +123,36 @@ make_graph <- function(data){
       #geom_text(aes(x = 40000000, y = 20000000, label = glue('r = {corl[1,2]}')), col = '#BC679B') +
       #geom_text(aes(x = 40000000, y = 19000000, label = glue('r = {corl[2,2]}')), col = '#3E51E3') +
       #geom_smooth(method = 'lm', se = TRUE) +
-      geom_text(aes(label = 'Predicted Abundance'))+
       geom_abline(intercept = 0, slope = 1, colour = 'black') +
-      geom_abline( intercept = 0, slope = 0.5,colour = 'darkgrey', linetype = 'dashed') +
-     # geom_abline( intercept = -5000000, colour = 'darkgrey', linetype = 'dashed') +
-      geom_abline( intercept = 0, slope = 0.2, colour = 'darkgrey', linetype = 'dotted') +
-      #geom_abline( intercept = -2000000, colour = 'darkgrey', linetype = 'dotted') +
+      geom_abline(intercept = 0, slope = 0.5, colour = 'blue', linetype = 'dashed') +
+      geom_abline(intercept = 0, slope = 0.2, colour = 'red', linetype = 'dashed') +
+      geom_abline(intercept = 0, slope = 2, colour = 'green', linetype = 'dashed') +
       ggtitle(label = gcm[gc]) +
       theme_bw() +
-      scale_x_continuous(labels = scales::comma) +
-      scale_y_continuous(labels = scales::comma)+
-      theme(plot.title = element_text(size = 14, face = 'bold'),
-            axis.text = element_text(size = 10),
-            axis.title.x = element_text(size = 12, face = 'bold'),
-            axis.title.y = element_text(size = 12, face = 'bold'),
-            axis.text.y = element_text(angle = 90, vjust = 0.5,  hjust = 0.5),
+      scale_x_continuous(labels = scales::scientific) +
+      scale_y_continuous(labels = scales::scientific)+
+      theme(plot.title = element_text(size = 16, face = 'bold'),
+            axis.title.x = element_text(size = 18, face = 'bold'),
+            axis.title.y = element_text(size = 18, face = 'bold'),
+            axis.text.y = element_text(angle = 90, vjust = 0.5,  hjust = 0.5, size = 16),
+            axis.text.x = element_text(size = 16),
             aspect.ratio = 1,
-            legend.position = 'right') +
-      labs(x = 2011, y = 2031, col = 'Group') +
+            legend.position = 'bottom',
+            legend.text = element_text(size= 12)) +
+      labs(x = 2011, y = 2091, col = 'Group') 
       
     
-    ggsave(plot = gsct, filename = glue('./graphs/figs/scatter/scatter_abund1131newslope_{gcm[gc]}.png'),  ## the notation is not scientific
+    ggsave(plot = gsct, filename = glue('./graphs/figs/scatter/group_abund1191newslopes72_{gcm[gc]}.png'),  ## the notation is not scientific
            units = 'in', width = 12, height = 9, dpi = 700)
     
     return(gsct)
   })
 }
-corPlot <- make_graph(data = sum)
+corPlot <- make_graph(data = data)
 
 # Join all into only one
-gall <- ggpubr::ggarrange(g1g2, g2g3, g2g3, ncol = 1, nrow = 3)
+gall <- ggpubr::ggarrange(corPlot, ncol = 1, nrow = 3)
+gall <- gridExtra::grid.arrange(grobs = corPlot)
 yr1yr2 <- make_graph(data = sum)
 
 # Join all into only one
