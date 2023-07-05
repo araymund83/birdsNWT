6e6c8fa8c0b9aa94f4a9d9c5dd3e9506b29e673c
Ana Ray <araymund83@gmail.com>
2023-07-05 09:04:02 -0700

    feat- LSD plot and results for the anova analysis in the mean zonal p_occ

--- anova_zonal.R
+++ anova_zonal.R
@@ -1,8 +1,6 @@
 # Load libraries ----------------------------------------------------------
 require(pacman)
-pacman::p_load(raster, rgdal, rgeos, reproducible, RColorBrewer, ggspatial, 
-               ggpubr, gridExtra, stringr, glue, sf, tidyverse, fasterize,
-               RStoolbox, fs, fst, trend, colorspace, hrbrthemes,exactextractr, furrr, future, spatialEco)
+pacman::p_load(dplyr,emmeans,glue,multcomp, qs, tidyr,tidyverse)
 
 g <- gc(reset = TRUE)
 rm(list = ls())
@@ -11,11 +9,12 @@ rm(list = ls())
 root <- './outputs'
 dirs <- fs::dir_ls(root, type = 'directory')
 spcs <- basename(dirs)
+spcs <- spcs[1:72]
 
 # Function to use ---------------------------------------------------------
 logZonalDF <- function(spc){
     
-    #spc <- spcs[1]
+    spc <- spcs[1]
     cat('-------------------------------------------------------------\n')
     cat('To start ', spc, '\n')
     cat('-------------------------------------------------------------\n')
@@ -35,34 +34,159 @@ znl <- bind_rows(zonalDF)
 
 
 #qs::qsave(x = znl, file = glue('./qs/zonal/allSpp_logZonal2_region2.qs'))
-df <- qs::qread(file = glue('./qs/zonal/allSpp_logZonal2_region2.qs'))
-df <- df %>% group_by(specie, model) %>% mutate(region2 = recode(region, 'High Boreal' = 'South',
-                        'Mid-Boreal' = 'South', 'High Subarctic' = 'North',
-                        'Low Arctic north' = 'North',
-                        'Low Subarctic' = 'Middle'
-                        ) )
-df <- df %>% group_by(specie, model) %>% 
-    mutate(region2 =factor(region2, levels = c('South', 'Middle', 'North')),
-           model = factor(model))
+df <- qs::qread(file = glue('./qs/zonalOccPi/all_sp_occPiZonal1191.qs'))
 
-df$region2 <- relevel(df$region2, ref = 'Middle')
+df <- df %>% group_by(specie, model) %>% 
+    mutate(region =as.factor(region),
+           model = as.factor(model))
+
+df$region <- relevel(df$region, ref = 'Middle')
+
 # Calculate test statistics using aov function
-mod1 <- lm(average~ region2 + model, data = df)
+mod1 <- lm (average~ region + model, data = df)
+mod2 <- lm (average~ region * model, data = df)
 anova(mod1)
 print(summary(mod1))
 coef(summary.lm(mod1))
-leastSquare <- emmeans(mod1, pairwise~region2, adjust = 'tukey')
+leastSquare2 <- emmeans::emmeans(mod1, pairwise~ region, adjust = 'tukey')
+
+#all pairwise comparison 
+m1_emm <- emmeans::emmeans(mod1, specs = pairwise ~ region:model)
+marginal <- emmeans::emmeans(mod1, ~ region)
+CLD = multcomp::cld(marginal, alpha = 0.05,
+                    letters = letters,
+                    adjust = 'tukey')
+pairs(marginal, adjust = 'tukey')
+marginal2 <- marginal %>% as.data.frame()
+
+#put results in a df
+meansDT<-m1_emm$emmeans %>% as.data.frame()
+m1_simple <- emmeans::contrast(m1_emm,
+                      method = "pairwise",
+                      simple = "each",
+                      combine = TRUE,
+                      adjust = "tukey") %>%
+  summary(infer = TRUE)
+
+##now create plot of the mean effects 
+my_colors <- c("#FF6A00", "#C15CCB",  "#00868B")
+
+meanPlot<- ggplot() + 
+  geom_point(data = meansDT, aes(x = region, y = emmean, group = model, color = model),
+  position = position_dodge(width = 0.8)) +
+  geom_errorbar(data = meansDT, aes(x = region, ymin = lower.CL, ymax = upper.CL, 
+                                    color = model), width = 0.2,
+                position = position_dodge(width = 0.8)) +
+  labs(x = "Zone", y = "Estimate", color = "") +
+  scale_x_discrete(limits = c('North', 'Middle', 'South')) +
+  scale_color_manual(values = my_colors) +  # Assign custom colors
+  theme_bw() +
+  theme(plot.title = element_text(size = 14, face = 'bold'),
+        axis.title.x = element_text(size = 12, face = 'bold'),
+        axis.title.y = element_text(size = 12, face = 'bold'),
+        axis.text.y = element_text( vjust = 0.5,  hjust = 0.5, size = 11),
+        axis.text.x = element_text( vjust = 0.5,  hjust = 0.5, size = 11),
+        aspect.ratio = 1,
+        legend.position = 'right',
+        legend.text = element_text(size= 11)) 
+
+meanPlot2<- ggplot() + 
+  geom_point(data = marginal2, aes(x = region, y = emmean),
+             position = position_dodge(width = 0.8)) +
+  geom_errorbar(data = marginal2, aes(x = region, ymin = lower.CL, ymax = upper.CL),
+                width = 0.2,
+                position = position_dodge(width = 0.8)) +
+  labs(x = "Zone", y = "Estimate", color = "") +
+  scale_x_discrete(limits = c('North', 'Middle', 'South')) +
+  scale_color_manual(values = my_colors) +  # Assign custom colors
+  theme_bw() +
+  theme(plot.title = element_text(size = 14, face = 'bold'),
+        axis.title.x = element_text(size = 12, face = 'bold'),
+        axis.title.y = element_text(size = 12, face = 'bold'),
+        axis.text.y = element_text( vjust = 0.5,  hjust = 0.5, size = 11),
+        axis.text.x = element_text( vjust = 0.5,  hjust = 0.5, size = 11),
+        aspect.ratio = 1,
+        legend.position = 'right',
+        legend.text = element_text(size= 11)) 
+  
+out <- glue('./graphs/figs/zonal_emmeans/effectsregion_GCM.png')
+ggsave(plot = meanPlot, filename = out, units = 'in', width = 5, height = 4, dpi = 300)
+
+
+m1_response_fac <- ggstripchart(
+  data = df,
+  x = "model",
+  y = "average",
+  color = "region",
+  fill = "region",
+  ylab = 'Mean proportional change',
+ # palette = pal_okabe_ito_blue,
+  #position = position_dodge(width = dodge_width),
+  position = position_jitterdodge(dodge.width = dodge_width,
+                                  jitter.width = jitter_width)
+) 
+
+m1_response_fac + 
+  
+# add layer containing means
+ gg <- ggplot(data = m1_emm_dt, (aes(x = model, y = emmean), group = region)) +
+ gg + geom_point(aes(x = model, y = emmean,
+                 fill = model), 
+             size = 3) 
+
+m1_response_fac  +
+  
+  # add layer containing error bars
+  geom_errorbar(data = m1_emm_dt, 
+                aes(y = emmean,
+                    ymin = lower.CL, 
+                    ymax = upper.CL,
+                  color = region),
+                width = 0.05)
+               
+  
+  # add p-value brackets
+  stat_pvalue_manual(
+    data = m1_simple_dt,
+    label = "p_pretty",
+    xmin = "xmin",
+    xmax = "xmax",
+    y.position = c(3300, 3300, 3450, 3600),
+    tip.length = 0.01,
+    size = 3) +
+  
+
+
+
+
+
+
+
+plot(leastSquare[[1]],
+     CIs = TRUE, 
+     PIs = TRUE, 
+     comparisons = TRUE,  
+     colors= c("black","dark grey","grey","green"),
+     alpha=0.05,
+     adjust="tukey") +
+  theme_bw() +
+ coord_flip()
 
 # display the results by grouping using letters
 pairs(leastSquare)
 plot(leastSquare, comparisons = TRUE)
-pwpp(mod1)
+
+emmeans::emmip(leastSquare, average ~ model * region, CIs = TRUE)
+
+emeansPlot <- leastSquare[[1]] %>%  as.data.frame()
+
 
 library("ggpubr")
-ggboxplot(df, x = "region2", y = "average", fill = 'model', xlab = 'Region', ylab = 'Mean proportional change', order = c('South', 'Middle', 'North'))
+ggboxplot(df, x = "model", y = "average", fill = 'region', xlab = 'Model', 
+          ylab = 'Mean proportional change')
 
 # compute summary statistics ----------------------------------------------
-group_by(df, region2, model) %>%
+group_by(df, region, model) %>%
     summarise(
         count = n(),
         mean = mean(average, na.rm = TRUE),
